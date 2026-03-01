27908f60342a925867c0e6cb01a6fe917762e142
kmerchant1 <kaiden.merchant@gmail.com>
2026-03-01 14:40:12 -0800

    register missing apis

--- src/api/routers/__init__.py
+++ src/api/routers/__init__.py
@@ -10,6 +10,9 @@ from src.api.routers.resume import router as resume_router
 from src.api.routers.skills import router as skills_router
 from src.api.routers.comparison import router as comparison_router
 from src.api.routers.linkedin import router as linkedin_router
+from src.insights.api import router as insights_router
+from src.insights.user_role_api import router as user_role_router
+from src.projects.api import router as thumbnail_router
 
 api_router = APIRouter()
 api_router.include_router(privacy_router)
@@ -20,5 +23,8 @@ api_router.include_router(resume_router)
 api_router.include_router(skills_router)
 api_router.include_router(comparison_router)
 api_router.include_router(linkedin_router)
+api_router.include_router(insights_router)
+api_router.include_router(user_role_router)
+api_router.include_router(thumbnail_router)
 
 __all__ = ["api_router"]
